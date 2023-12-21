56031b7065f97bfd999c432b2eb560525a0ce441
Riccardo <riccardocroce129@outlook.com>
2023-12-21 14:03:21 +0100

    Functionality: Join Room by ID

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,17 @@
 import { useContext, useState } from "react";
 import { StoreContext } from "../provider/StoreProvider";
 import tenebra from "../assets/tenebralfgo.gif";
-import { Collapse, Tooltip } from "@mui/material";
+import { Collapse } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuid4 } from "uuid";
 import InputNickname from "./InputNickname";
+import GenericInput from "./GenericInput";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const { I18N, user } = useContext(StoreContext);
   const [isUserLogged, setUserLogged] = useState(false);
+  const [isJoining, setJoining] = useState(false);
   const handleUserLogin = () => {
     setUserLogged(true);
   };
@@ -17,7 +19,9 @@ const Dashboard = () => {
     const room = uuid4();
     navigate(`room/${room}`);
   };
-  const handleJoinRoom = () => {};
+  const handleJoinRoom = () => {
+    setJoining(true);
+  };
   return (
     <div className="component">
       {!isUserLogged || !user ? (
@@ -31,20 +35,27 @@ const Dashboard = () => {
         ""
       )}
       <br />
-      <Collapse timeout={500} in={isUserLogged}>
+      <Collapse timeout={1000} in={isUserLogged}>
         {isUserLogged && (
           <div className="dashboardButtonsParent">
-            <button className="buttonBox" onClick={handleCreateNewRoom}>
-              {I18N.DASHBOARD.NEWROOM}
-            </button>
-            <h3 className="dashboardOr">{I18N.DASHBOARD.OR}</h3>
-            <Tooltip classes={{ tooltip: "popperTooltip", touch: "popperTooltip" }} title={I18N.MISC.BETA} placement="bottom">
-              <span>
-                <button className="buttonBox" onClick={handleJoinRoom} disabled>
+            {!isJoining && (
+              <>
+                <button className="buttonBox" onClick={handleCreateNewRoom}>
+                  {I18N.DASHBOARD.NEWROOM}
+                </button>
+                <h3 className="dashboardOr">{I18N.DASHBOARD.OR}</h3>
+              </>
+            )}
+            <span>
+              {!isJoining && (
+                <button className="buttonBox" onClick={handleJoinRoom}>
                   {I18N.DASHBOARD.JOINROOM}
                 </button>
-              </span>
-            </Tooltip>
+              )}
+              <Collapse timeout={1000} in={isJoining}>
+                {isJoining && <GenericInput />}
+              </Collapse>
+            </span>
           </div>
         )}
       </Collapse>
